e67fbf9007024faebad7cb664d700cd2eaaa9788
Alejandro <luisguerrero12387@gmail.com>
2022-02-09 00:02:44 -0400

    adding the necessary components and modules

---
+++ hogwartTestFrontend/src/app/components/table-characters/table-characters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-characters',
+  templateUrl: './table-characters.component.html',
+  styleUrls: ['./table-characters.component.css']
+})
+export class TableCharactersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
